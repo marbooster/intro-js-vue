27954ee88de923c1397d7e0d08916a074ac3be7c
marcelin <marcelin.tagba@gmail.com>
2023-01-12 14:29:34 +0100

    5e commit

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/intro-js-vue/",
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@":  fileURLToPath(new URL('./src', import.meta.url)),
       vue: '@vue/compat',
-    }
+    },
   },
-  base: '/intro-js-vue/',
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
+

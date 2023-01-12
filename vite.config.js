ba63881dec57d98113d89a90f75971224ce32f6a
marcelin <marcelin.tagba@gmail.com>
2023-01-12 14:41:42 +0100

    6e commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/intro-js-vue/",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
